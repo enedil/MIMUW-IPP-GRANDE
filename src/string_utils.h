690dc47c4d644f077774153da76ce502b4623dbc
Michał Radwański <enedil@outlook.com>
2019-04-27 19:06:25 +0200

    string utils abstracted out to a file

---
+++ src/string_utils.h
@@ -0,0 +1,9 @@
+#ifndef __STRING_UTILS__
+#define __STRING_UTILS__
+
+#include "dictionary.h"
+
+hash_t hash_string(void* str);
+bool undereferencing_strcmp(void* x, void* y);
+
+#endif /* __STRING_UTILS__ */
