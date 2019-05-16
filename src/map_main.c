ab4f185f21c68de976d3c08f812f99eb056b5285
Michał Radwański <enedil@outlook.com>
2019-05-16 16:28:08 +0200

    partial implementation of main module

--- src/map_main.c
+++ src/map_main.c
@@ -1,52 +1,30 @@
-#include "map.h"
+// needed for getline
+#define _XOPEN_SOURCE 700
 
+#include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#undef NDEBUG
-
-#include <assert.h>
+#include "map.h"
+#include "parser.h"
 
 int main() {
-  char const* str;
+    Map* m = newMap();
+    if (m == NULL) {
+        return 0;
+    }
 
-  Map* m = newMap();
-  assert(m);
+    char* line = NULL;
+    size_t line_len = 0;
+    while (getline(&line, &line_len, stdin) != -1) {
+        if (errno == ENOMEM) {
+            exit(0);
+        }
+        struct operation op = parse(line, strlen(line));
+        printf("%d %s\n", op.op, op.arg);
+    }
 
-  assert(addRoad(m, "Alinów", "Bór", 1, 2020));
-  assert(addRoad(m, "Bór", "Cielińsk-Niekłańsk", 2, 2020));
-  assert(addRoad(m, "Bór", "Dąb Stary", 3, 2020));
-  assert(addRoad(m, "Cielińsk-Niekłańsk", "Emiliew", 4, 2020));
-  assert(addRoad(m, "Dąb Stary", "Emiliew", 5, 2020));
-  assert(addRoad(m, "Emiliew", "Bór", 8, 2020));
-  assert(addRoad(m, "Emiliew", "Fraźnik Nowy", 3, 2020));
-  assert(!repairRoad(m, "Emiliew", "Cielińsk-Niekłańsk", 2019));
-  assert(repairRoad(m, "Emiliew", "Cielińsk-Niekłańsk", 2021));
-  assert(!repairRoad(m, "Emiliew", "Alinów", 2020));
-  assert(addRoad(m, "Fraźnik Nowy", "Grzegrzewo", 4, 2020));
-  assert(addRoad(m, "Alinów", "Grzegrzewo", 10, 2020));
-  assert(addRoad(m, "Homilcin", "Grzegrzewo", 5, 2020));
-  assert(addRoad(m, "Fraźnik Nowy", "Cielińsk-Niekłańsk", 2, 2020));
-  assert(!addRoad(m, "Fraźnik Nowy", "Cielińsk-Niekłańsk", 2, 2020));
-  assert(!addRoad(m, "Cielińsk-Niekłańsk", "Fraźnik Nowy", 2, 2020));
-  assert(!repairRoad(m, "Emiliew", "Bór", 2018));
-  assert(repairRoad(m, "Emiliew", "Cielińsk-Niekłańsk", 2021));
-  assert(repairRoad(m, "Emiliew", "Fraźnik Nowy", 2023));
-  assert(addRoad(m, "Homilcin", "Cielińsk-Niekłańsk", 3, 2020));
-  assert(newRoute(m, 10, "Alinów", "Emiliew"));
-
-  str = getRouteDescription(m, 10);
-  assert(strcmp(str, "10;Alinów;1;2020;Bór;2;2020;Cielińsk-Niekłańsk;4;2021;Emiliew") == 0);
-  free((void *)str);
-
-  assert(extendRoute(m, 10, "Homilcin"));
-
-  str = getRouteDescription(m, 10);
-  assert(strcmp(str, "10;Alinów;1;2020;Bór;2;2020;Cielińsk-Niekłańsk;4;2021;Emiliew"
-                     ";3;2023;Fraźnik Nowy;4;2020;Grzegrzewo;5;2020;Homilcin") == 0);
-  free((void *)str);
-
-  deleteMap(m);
-
-  return 0;
+    deleteMap(m);
+    return 0;
 }
