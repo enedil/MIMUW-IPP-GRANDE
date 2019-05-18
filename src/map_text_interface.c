5126aac933cdc2697b7ffb6dc19f919e6c96bb9b
Michał Radwański <enedil@outlook.com>
2019-05-18 03:16:44 +0200

    map_text_interface

---
+++ src/map_text_interface.c
@@ -0,0 +1,184 @@
+#include <stdio.h>
+#include "map_text_interface.h"
+#include "utils.h"
+
+Status execAddRoad(Map* map, char* arg) {
+    Status ret = false;
+    char *city1 = NULL, *city2 = NULL;
+    CHECK_RET(extractCityName(arg, &city1));
+    arg = strchr(arg, ';') + 1;
+    if (!extractCityName(arg, &city2)) {
+        goto CLEANUP;
+    }
+    arg = strchr(arg, ';') + 1;
+    unsigned long long length;
+    if (!extractRoadLength(arg, &length)) {
+        goto CLEANUP;
+    }
+    arg = strchr(arg, ';') + 1;
+    int year;
+    if (!extractYear(arg, &year)) {
+        goto CLEANUP;
+    }
+
+    ret = addRoad(map, city1, city2, (unsigned)length, year);
+CLEANUP:
+    free(city1);
+    free(city2);
+    return ret;
+}
+
+Status execRepairRoad(Map* map, char* arg) {
+    Status ret = false;
+    char *city1 = NULL, *city2 = NULL;
+    CHECK_RET(extractCityName(arg, &city1));
+    arg = strchr(arg, ';') + 1;
+    if (!extractCityName(arg, &city2)) {
+        goto CLEANUP;
+    }
+    arg = strchr(arg, ';') + 1;
+    int year;
+    if (!extractYear(arg, &year)) {
+        goto CLEANUP;
+    }
+    ret = repairRoad(map, city1, city2, year);
+CLEANUP:
+    free(city1);
+    free(city2);
+    return ret;
+}
+
+Status execGetRouteDescription(Map* map, char* arg) {
+    unsigned routeId;
+    CHECK_RET(extractRouteId(arg, &routeId));
+    const char* s = getRouteDescription(map, routeId);
+    if (s != NULL) {
+        printf("%s\n", s);
+        free((void*)s);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+Road getRoadFromName(Map* map, char* city1, char* city2) {
+    Entry e1 = getDictionary(&map->city_to_int, (void *)city1);
+    Entry e2 = getDictionary(&map->city_to_int, (void *)city2);
+    if (NOT_FOUND(e1) || NOT_FOUND(e2)) {
+        return (const Road){0};
+    }
+    int id1 = decodeCityId(e1.val);
+    int id2 = decodeCityId(e2.val);
+    return getRoad(map, id1, id2);
+}
+
+/** @brief Zwraca wskaźnik po n-tym średniku w stringu.
+ * @param[in] ptr     - string, który przeszukujemy
+ * @param[in] n       - numer średnika
+ * @return Wskaźnik po n-tym średniku w stringu, lub NULL, jeśli taki nie istnieje.
+ */
+static char* nextNthSemicolon(char* ptr, size_t n) {
+    for (size_t i = 0; i < n && ptr-1; ++i) {
+        ptr = strchr(ptr, ';') + 1;
+    }
+    return ptr == (void*)1 ? NULL : ptr;
+}
+
+Status execNewRoute(Map* map, char* arg) {
+    if (*arg < '0' || *arg > '9') {
+        return false;
+    }
+    unsigned routeId;
+    CHECK_RET(extractRouteId(arg, &routeId));
+    if (!isEmptyList(&map->routes[routeId].cities)) {
+        return false;
+    }
+
+    char* prev = nextNthSemicolon(arg, 1);
+    char* ptr = nextNthSemicolon(prev, 3);
+
+    // check if every edge can be inserted
+    while (prev && ptr) {
+        char *x = NULL, *y = NULL;
+        if (!extractCityName(prev, &x) || !extractCityName(ptr, &y)) {
+            free(x);
+            free(y);
+            return false;
+        }
+        Road r = getRoadFromName(map, x, y);
+        if (r.length != 0) {
+
+            unsigned long long len;
+            int year;
+            if (!extractRoadLength(nextNthSemicolon(prev, 1), &len)
+                    || len != r.length
+                    || !extractYear(nextNthSemicolon(prev, 2), &year)
+                    || year < r.builtYear) {
+                free(x);
+                free(y);
+                return false;
+            }
+        }
+        free(x);
+        free(y);
+        prev = nextNthSemicolon(prev, 3);
+        ptr = nextNthSemicolon(ptr, 3);
+    }
+
+    List *l = newList();
+    CHECK_RET(l);
+    map->routes[routeId].cities = *l;
+    free(l);
+
+    prev = nextNthSemicolon(arg, 1);
+    char* x;
+    if (!extractCityName(prev, &x)) {
+        free(x);
+        return false;
+    }
+    if (!addCity(map, x)) {
+        free(x);
+        return false;
+    }
+    Entry e = getDictionary(&map->city_to_int, x);
+    int id = decodeCityId(e.val);
+    listInsertAfter(&map->routes[routeId].cities, map->routes[routeId].cities.begin, id);
+    free(x);
+    ptr = nextNthSemicolon(prev, 3);
+    while (prev && ptr) {
+        char *x = NULL, *y = NULL;
+        if (!extractCityName(prev, &x) || !extractCityName(ptr, &y)) {
+            free(x);
+            free(y);
+            return false;
+        }
+        unsigned long long length;
+        int year;
+        if (extractRoadLength(nextNthSemicolon(prev, 1), &length)
+                && extractYear(nextNthSemicolon(prev, 2), &year)) {
+            if (!addRoadRepair(map, x, y, (unsigned)length, year)) {
+                free(x);
+                free(y);
+                return false;
+            }
+        }
+        Road r = getRoadFromName(map, x, y);
+        if (!listInsertAfter(&map->routes[routeId].cities, map->routes[routeId].cities.end, r.end)) {
+            return false;
+        }
+        Entry e = getDictionary(&map->routesThrough, encodeEdgeAsPtr(r.start, r.end));
+        if (NOT_FOUND(e)) {
+            return false;
+        }
+        List* routesthrough = e.val;
+        if (!listInsertAfter(routesthrough, routesthrough->end, routeId)) {
+            return false;
+        }
+
+        free(x);
+        free(y);
+        prev = nextNthSemicolon(prev, 3);
+        ptr = nextNthSemicolon(ptr, 3);
+    }
+    return true;
+}
---
+++ src/map_text_interface.h
@@ -0,0 +1,40 @@
+#ifndef __MAP_TEXT_INTERFACE_H__
+#define __MAP_TEXT_INTERFACE_H__
+
+#include <string.h>
+#include <stdlib.h>
+#include "status.h"
+#include "parser.h"
+#include "map.h"
+
+/** @brief Dodaje drogę
+ * @param[in,out] map   - mapa, do której dodajemy drogę
+ * @param[in] arg       - string w formacie wejściowym
+ * @return
+ */
+Status execAddRoad(Map* map, char* arg);
+
+/** @brief Naprawia drogę
+ * @param[in,out] map   - mapa, z której naprawiamy drogę
+ * @param[in] arg       - string w formacie wejściowym
+ * @return
+ */
+Status execRepairRoad(Map* map, char* arg);
+
+/** @brief Wyświetla drogę krajową.
+ * Jeśli droga nie istnieje, nic nie robi.
+ * @param[in,out] map   - mapa, z której wyświetlamy drogę krajową
+ * @param[in] arg       - string w formacie wejściowym
+ * @return
+ */
+Status execGetRouteDescription(Map* map, char* arg);
+
+/** Dodaje drogę krajową
+ * @brief execNewRoute
+ * @param[in,out] map   - mapa, do której dodajemy drogę krajową
+ * @param[in] arg       - string w formacie wejściowym
+ * @return
+ */
+Status execNewRoute(Map* map, char* arg);
+
+#endif /* __MAP_TEXT_INTERFACE_H__ */
